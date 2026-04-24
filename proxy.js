b890f861920cf8d81d9ea3717f39bc80864f4296
AlexeyGallegoMartinez <alexeygallegomartinez@gmail.com>
2026-04-24 06:26:50 -0400

    rebrand fix v4

--- proxy.js
+++ proxy.js
@@ -5,20 +5,18 @@ import { fallbackLng, languages, cookieName } from "./app/i18n/settings";
 acceptLanguage.languages(languages);
 
 export const config = {
-  // matcher: '/:lng*'
   matcher: [
     "/((?!api|_next/static|_next/image|assets|favicon.ico|sw.js|site.webmanifest|.*\\..*).*)",
   ],
 };
 
-export function middleware(req) {
+export function proxy(req) {
   let lng;
   if (req.cookies.has(cookieName))
     lng = acceptLanguage.get(req.cookies.get(cookieName).value);
   if (!lng) lng = acceptLanguage.get(req.headers.get("Accept-Language"));
   if (!lng) lng = fallbackLng;
 
-  // Redirect if lng in path is not supported
   if (
     !languages.some((loc) => req.nextUrl.pathname.startsWith(`/${loc}`)) &&
     !req.nextUrl.pathname.startsWith("/_next")
